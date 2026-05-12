cd825ac0017d77bb70ed28ec27176416d388c65d
23manuel <emmanuel.okon23@outlook.com>
2026-05-12 11:20:21 +0100

    Deployment: Final port and host config

--- novapay_backend/settings.py
+++ novapay_backend/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-$0ynf660p(rmp&va9asus5_tuorx_n_5!f#3*y6anox7ih=q9t
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ['customer-agent-pro.onrender.com', 'localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['*']  # This is the "No-Wahala" setting for now
+# ALLOWED_HOSTS = ['customer-agent-pro.onrender.com', 'localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
